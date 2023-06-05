5580b354fbc9f7fb4d9b9413b7bb0de161c12bca
Undefined <theundefined2001@gmail.com>
2023-06-06 05:45:01 +0600

    More Unit Added

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-let button = document.getElementById("btn");
+// Get references to the select element and the input element
+const operationSelect = document.getElementById("option");
+const inputElement = document.getElementById("inp");
+const outputField = document.getElementById("res");
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    var input = document.getElementById("inp");
-    input.placeholder = "Enter Your Number(m²)";
+// Add event listener to the button
+const button = document.getElementById("btn");
+button.addEventListener("click", () => {
+    // Get the selected option value
+    const selectedOption = operationSelect.value;
+    
+    // Get the input value
+    const inputValue = parseFloat(inputElement.value);
+    
+    // Perform the corresponding operation based on the selected option
+    switch (selectedOption) {
+        case "mtf":
+            outputField.innerHTML = outputFormat(meterToFeet(inputValue), "foot");
+            break;
+        case "smtsf":
+            outputField.innerHTML = outputFormat(meterToFeet(inputValue, false, true), " sq. foot");
+            break;
+        case "ftm":
+            outputField.innerHTML = outputFormat(meterToFeet(inputValue, true, false), "meter");
+            break;
+        default:
+            console.log("Invalid operation");
+            break;
+    }
 });
 
-const readInput = (event) => {
-    event.preventDefault();
-    let value = document.getElementById("inp").value;
-    let resinp = document.getElementById("res");
-    let result = value * Math.pow(3.28084, 2) ;
-    console.log(result);
-    resinp.innerHTML = result.toFixed(3) + " Sq. Foot";
-
+function meterToFeet(num, reverse = false, is_sq = false) {
+    const conversionFactor = 3.28084;
+    const multiplier = reverse ? 1 / conversionFactor : conversionFactor;
+    const result = is_sq ? num * multiplier * multiplier : num * multiplier;
+    return result;
 }
 
-button.addEventListener("click", readInput);
-
+function outputFormat(val, unit) {
+    let temp = unit.charAt(0).toUpperCase() + unit.slice(1);
+    return `Your result is:<br> ${val.toFixed(3)} ${temp}`;
+}
